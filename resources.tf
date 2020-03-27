cc39c327b2d554d1b1a8ebe2d77479764648e2e8
dograr <rajesh.dogra@gmail.com>
2020-03-27 14:26:34 +0530

    Adding the latest changes

--- resources.tf
+++ resources.tf
@@ -34,3 +34,15 @@ resource "aws_instance" "db" {
   }
 }
 
+resource "aws_instance" "db1" {
+  ami = "${var.ami}"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.default.id}"
+  subnet_id = "${aws_subnet.private_subnet2.id}"
+  vpc_security_group_ids = ["${aws_security_group.sgdb2.id}"]
+  source_dest_check = false
+
+  tags = {
+    Name = "database"
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -8,6 +8,21 @@ resource "aws_vpc" "default" {
   }
 }
 
+resource "aws_vpc" "default1" {
+  cidr_block = "${var.vpc_2_cidr}"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "dograr-2-vpc"
+  }
+}
+
+
+resource "aws_vpc_peering_connection" "test" {
+  peer_vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "${aws_vpc.default1.id}"
+}
+
 #Define the public subnet
 resource "aws_subnet" "public_subnet" {
   cidr_block = "${var.public_subnet_cidr}"
@@ -20,22 +35,32 @@ resource "aws_subnet" "public_subnet" {
 }
 
 #Define the private subnet
+
 resource "aws_subnet" "private_subnet" {
   cidr_block = "${var.private_subnet_cidr}"
   vpc_id = "${aws_vpc.default.id}"
   availability_zone = "us-east-2b"
-  
-  tags = {
-    Name="db private Subnet"
+  tags  = {
+    Name="db-private-Subnet"
   }
 }
+
+resource "aws_subnet" "private_subnet2" {
+  cidr_block = "${var.private_subnet_2_cidr}"
+  vpc_id = "${aws_vpc.default1.id}"
+  availability_zone = "us-east-2b"
+  tags  = {
+    Name="db-private-Subnet-2"
+  }
+}
+
 
 #Define the internt gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.default.id}"
 
   tags = {
-    Name = "VPC IGW"
+    Name = "VPC-IGW"
   }
 }
 
@@ -45,29 +70,56 @@ resource "aws_route_table" "web-public-rt" {
   route  {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.gw.id}"
-	}
-    tags =  {
-      Name =  "Public Subnet RT"
-    }
+  }
+  tags = {
+    Name =  "Public-Subnet-RT"
+  }
+
 }
 
 #Define the route table
 resource "aws_route_table" "db-private-rt" {
   vpc_id = "${aws_vpc.default.id}"
-  route  {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
-	}
-    tags = {
-      Name =  "Private Subnet RT"
-    }
+  route{
+    cidr_block = "10.200.2.0/24"
+    gateway_id = "${aws_vpc_peering_connection.test.id}"
+  }
+  tags = {
+    Name =  "Private-Subnet-RT"
+  }
+
 }
+
+resource "aws_route_table" "db-private-rt2" {
+  vpc_id = "${aws_vpc.default1.id}"
+  route{
+    cidr_block = "10.100.2.0/24"
+    gateway_id = "${aws_vpc_peering_connection.test.id}"
+  }
+  tags = {
+     Name = "Private-Subnet-RT-2"
+  }
+}
+
 
 #assigne the route table to the public subnet
 resource "aws_route_table_association" "web-public-rt" {
   route_table_id = "${aws_route_table.web-public-rt.id}"
   subnet_id = "${aws_subnet.public_subnet.id}"
 }
+
+resource "aws_route_table_association" "db-private-rt" {
+  route_table_id = "${aws_route_table.db-private-rt.id}"
+  subnet_id = "${aws_subnet.private_subnet.id}"
+}
+
+resource "aws_route_table_association" "db-private-rt2" {
+  route_table_id = "${aws_route_table.db-private-rt2.id}"
+  subnet_id = "${aws_subnet.private_subnet2.id}"
+}
+
+
+
 
 #Define security group for public subnet
 resource "aws_security_group" "sgweb" {
@@ -122,13 +174,6 @@ resource "aws_security_group" "sgdb"{
   description = "Allow traffic from public subnet"
 
   ingress {
-    from_port = 3306
-    to_port = 3306
-    protocol = "tcp"
-    cidr_blocks = ["${var.public_subnet_cidr}"]
-  }
-
-  ingress {
     from_port = -1
     to_port = -1
     protocol = "icmp"
@@ -148,3 +193,29 @@ resource "aws_security_group" "sgdb"{
     Name = "DB SG"
   }
 }
+
+resource "aws_security_group" "sgdb2"{
+  name = "sg_test_web2"
+  description = "Allow traffic from public subnet"
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.private_subnet_cidr}"]
+  }
+
+  vpc_id = "${aws_vpc.default1.id}"
+
+  tags = {
+    Name = "DB SG2"
+  }
+}
+
